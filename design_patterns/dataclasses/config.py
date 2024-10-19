9499415ae074e19be9f5e6e82f1db78ffeb7c384
Daniel Gil <jodagito@gmail.com>
2024-10-19 10:13:13 -0500

    Adds configs for smtp

--- design_patterns/dataclasses/config.py
+++ design_patterns/dataclasses/config.py
@@ -7,3 +7,8 @@ class Config:
     sinch_number: str
     sinch_service_plan_id: str
     sinch_url: str
+    smtp_server: str
+    smtp_port: int
+    smtp_login: str
+    smtp_password: str
+    default_smtp_email: str
