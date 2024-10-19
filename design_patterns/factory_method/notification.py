64d1e3886c4e13e90a3c69df0d818763ccfc39ea
Daniel Gil <jodagito@gmail.com>
2024-10-18 20:44:02 -0500

    Updates base Notification and SMSNotification classes to use ConfigsLoader to retrieve needed values

--- design_patterns/factory_method/notification.py
+++ design_patterns/factory_method/notification.py
@@ -3,12 +3,17 @@ from abc import (
     abstractmethod,
 )
 
+from design_patterns.singleton.config import ConfigsLoader
+
 
 class Notification(ABC):
+    def __init__(self):
+        self.configs = ConfigsLoader().get_configs()
+
     @abstractmethod
     def send_notification(self) -> None:
         pass
 
     @abstractmethod
-    def set_message(self, message: str) -> None:
+    def set_message_payload(self, message: str) -> None:
         pass
--- design_patterns/factory_method/sms_notification.py
+++ design_patterns/factory_method/sms_notification.py
@@ -8,20 +8,17 @@ class InvalidSinchResponse(Exception):
 
 
 class SMSNotification(Notification):
-    def __init__(self, service_plan_id: str | None = None,
-                 api_token: str | None = None,
-                 sinch_number: str | None = None,
-                 to_number: str | None = None):
-        self.service_plan_id = service_plan_id
-        self.api_token = api_token
-        self.sinch_number = sinch_number
-        self.to_number = to_number
+    def __init__(self):
+        super().__init__()
+        self.sinch_service_plan_id = self.configs.sinch_service_plan_id
+        self.sinch_api_token = self.configs.sinch_api_token
+        self.sinch_number = self.configs.sinch_number
 
-        self.url = "https://us.sms.api.sinch.com/xms/v1/"\
-            f"{self.service_plan_id}/batches"
+        self.url = self.configs.sinch_url.format(
+            sinch_service_plan_id=self.sinch_service_plan_id)
         self.headers = {
             "Content-Type": "application/json",
-            "Authorization": "Bearer " + str(self.api_token)
+            "Authorization": f"Bearer {self.sinch_api_token}"
         }
 
     def send_notification(self) -> None:
@@ -32,9 +29,10 @@ class SMSNotification(Notification):
         except AssertionError:
             raise InvalidSinchResponse(response.json())
 
-    def set_message(self, message: str) -> None:
+    def set_message_payload(self, message: str,
+                            receiver_numbers: list[str]) -> None:
         self.payload = {
             "from": self.sinch_number,
-            "to": [self.to_number],
+            "to": receiver_numbers,
             "body": message
         }
