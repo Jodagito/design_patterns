8cf9fa862e09198c7581778f43c807a1bbe1be2a
Daniel Gil <jodagito@gmail.com>
2024-10-18 20:45:30 -0500

    Implements new BaseTestCase

--- tests/singleton/test_config.py
+++ tests/singleton/test_config.py
@@ -1,12 +1,13 @@
 import os
-from unittest import TestCase
 from unittest.mock import patch
 
 from design_patterns.singleton.config import ConfigsLoader
 
+from tests.base_test_case import BaseTestCase
 
-class TestConfig(TestCase):
-    def setUp(self):
+
+class TestConfig(BaseTestCase):
+    def setUp(self) -> None:
         super().setUp()
         ConfigsLoader._instance = None
 
@@ -32,11 +33,11 @@ class TestConfig(TestCase):
         configs_loader = ConfigsLoader()
         configs_loader.load_configs()
         configs = configs_loader.get_configs()
-        assert configs.database_name == "test_value"
+        assert configs.sinch_api_token == "test_value"
 
     @patch.dict(os.environ, {'RUN_MODE': 'prod'})
     def test_run_mode_prod_env_var(self):
         configs_loader = ConfigsLoader()
         configs_loader.load_configs()
         configs = configs_loader.get_configs()
-        assert configs.database_name == "prod_value"
+        assert configs.sinch_api_token == "prod_value"
