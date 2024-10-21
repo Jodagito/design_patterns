76715ac4ce420827ed24cb3e7d74083a78019e15
Daniel Gil <jodagito@gmail.com>
2024-10-20 19:41:36 -0500

    Refactors base Notification class to support new way to send SMS notifications with AWS Lambda

--- design_patterns/factory_method/notification.py
+++ design_patterns/factory_method/notification.py
@@ -3,18 +3,25 @@ from abc import (
     abstractmethod,
 )
 
+import boto3
+
 from design_patterns.singleton.config import ConfigsLoader
 
 
 class Notification(ABC):
-    def __init__(self):
+    def __init__(self, client: str):
         self.configs = ConfigsLoader().get_configs()
         self.payload: dict = None
+        self.client = boto3.client(client)
 
     @abstractmethod
     def send_notification(self) -> None:
         pass
 
     @abstractmethod
-    def set_message_payload(self, message: str) -> None:
+    def _lambda_handler(self) -> None:
+        pass
+
+    @abstractmethod
+    def _set_message_payload(self, message: str) -> None:
         pass
