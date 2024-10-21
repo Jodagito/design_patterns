30e1b099caf4c719ea9cff5598f5231055f3586b
Daniel Gil <jodagito@gmail.com>
2024-10-21 17:59:32 -0500

    Adds configs to support push notifications

--- design_patterns/dataclasses/config.py
+++ design_patterns/dataclasses/config.py
@@ -4,5 +4,8 @@ from pydantic.dataclasses import dataclass
 @dataclass
 class Config:
     aws_region: str
-    sms_sender_number: str
-    default_source_email: str
+    sms_sender_number: str | None
+    default_source_email: str | None
+    android_application_arn: str | None
+    browser_application_arn: str | None
+    ios_application_arn: str | None
