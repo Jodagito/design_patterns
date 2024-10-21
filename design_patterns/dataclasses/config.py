da8ac521f742fcc9c26c6c1b197e7c6d819a57f8
Daniel Gil <jodagito@gmail.com>
2024-10-20 21:54:05 -0500

    Refactors configs to support email notifications with SES

--- design_patterns/dataclasses/config.py
+++ design_patterns/dataclasses/config.py
@@ -4,8 +4,4 @@ from pydantic.dataclasses import dataclass
 @dataclass
 class Config:
     sms_sender_number: str
-    smtp_server: str
-    smtp_port: int
-    smtp_login: str
-    smtp_password: str
-    default_smtp_email: str
+    default_source_email: str
